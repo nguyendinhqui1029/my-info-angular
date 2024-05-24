c330e63d177844f6aa063ad29c62c7dcd670114f
nguyendinhqui1029 <nguyendinhqui1029@gmail.com>
2024-05-24 12:51:13 +0700

    Create layout

--- angular-portfolio-client/src/app/app.routes.ts
+++ angular-portfolio-client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from '@pages/client/layout/layout.component';
+import { HomeComponent } from '@pages/client/home/home.component';
+import { AdminLayoutComponent } from '@pages/admin/layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { 
+        path: '', 
+        component: LayoutComponent, 
+        children: [
+            { path: '/home', component: HomeComponent }
+         ]
+    },
+    { path: 'admin', component: AdminLayoutComponent },
+  ];
