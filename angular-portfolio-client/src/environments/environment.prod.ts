5a26d7bfc4af446aa08a291bb47480ec09844497
nguyendinhqui1029 <nguyendinhqui1029@gmail.com>
2024-06-03 23:28:43 +0700

    update env pro

--- angular-portfolio-client/src/environments/environment.prod.ts
+++ angular-portfolio-client/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { Environment } from "@app/shared/models/environment.model";
 export const environment: Environment = {
     production: true,
     apiUrl: 'https://api.example.com',
-    isUseMock: false,
+    isUseMock: true,
     defaultLanguage: 'vi'
   };
