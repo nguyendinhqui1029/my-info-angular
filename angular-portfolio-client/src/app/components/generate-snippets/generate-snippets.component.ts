f1b9ca3047b8e81acf1e45ae2746f0e0d9ef170a
nguyendinhqui1029 <nguyendinhqui1029@gmail.com>
2024-06-29 08:12:56 +0700

    update generate snippets

--- angular-portfolio-client/src/app/components/generate-snippets/generate-snippets.component.ts
+++ angular-portfolio-client/src/app/components/generate-snippets/generate-snippets.component.ts
@@ -12,41 +12,47 @@ import { PrimeComponent } from '@app/configs/prime-angular/prime.config';
 })
 export class GenerateSnippetsComponent {
   valueForm = {
-    prefix: 'sgvnFontSize',
-    attributeCss: 'font-size',
     pixel: 16,
-    textAreaOne: '',
-    textAreaTwo: '',
-    textAreaThree: ''
+    values: [{
+      prefix: 'sgvnFontSize',
+      attributeCss: 'font-size',
+      textAreaOne: '',
+      textAreaTwo: '',
+      textAreaThree: ''
+    }]
   }
 
   error: string ='';
   result: any = {};
+  clipboardResult: any = {};
 
-  getListValueFromTextAreaOne() {
-    const result = this.valueForm.textAreaOne
+  getListValueFromTextAreaOne(value: string) {
+    if(!value) {
+      return [];
+    }
+    const result = value
     .replaceAll(/\s+/g, '')
-    .replaceAll(/((em|rem|px)\s*!default\s*;?)/g, ';')
+    .replaceAll(/\s*!default\s*;?/g, ';')
     .split(';').filter((item: string) => item);
     return result;
   }
 
-  getListValueFromTextAreaTwo() {
-    const startIndex = this.valueForm.textAreaTwo.indexOf('(');
-    const endIndex = this.valueForm.textAreaTwo.lastIndexOf(')') + 1;
+  getListValueFromTextAreaTwo(value: string) {
+    const startIndex = value.indexOf('(');
+    const endIndex = value.lastIndexOf(')') + 1;
     if(startIndex === -1 || endIndex === -1) {
       this.error = 'Textarea 2 sai format';
       return [];
     }
-    const result = this.valueForm.textAreaTwo.substring(startIndex + 1, endIndex - 1)
+    const result = value.substring(startIndex + 1, endIndex - 1)
     .replaceAll(/\s+/g, '')
     .split(',').filter((item: string) => item);
     return result;
   }
 
-  getListValueFromTextAreaThree() {
+  getListValueFromTextAreaThree(value: string) {
     let finalResult: string[] = [];
-    let result = this.valueForm.textAreaThree
+    let result = value
     .replaceAll(/\s+/g, '').split(';');
     result.forEach((item: string) => {
       const startVariable = item.indexOf('$');
@@ -63,50 +69,120 @@ export class GenerateSnippetsComponent {
    
     return finalResult;
   }
+
+  getValue(value: string) {
+    if(value.includes('rem') || value.includes('em')) {
+      return +value.replace('rem', '').replace('em', '') * this.valueForm.pixel;
+    }
+    if(value.includes('px')) {
+      return +value.replace('px', '');
+    }
+    return value;
+  }
+
+  getUnit(value: string) {
+    console.log('1',value)
+    if(value.includes('rem') || value.includes('em') || value.includes('px')) {
+      return true
+    }
+  
+    return false;
+  }
+
   handleGenerate() {
-    const objectMapOne: Record<string, {variable: string; value: number}> = {};
-    const resultOne = this.getListValueFromTextAreaOne();
-    resultOne.forEach((item: string) => {
-      const itemSplit = item.split(':');
-      if(itemSplit.length >= 2) {
-        const key = itemSplit[0].replace('$', '');
-        objectMapOne[key] = {
-          variable: itemSplit[0],
-          value: +itemSplit[1] * this.valueForm.pixel
+    this.result = {};
+    this.valueForm.values.forEach((item) => {
+      const objectMapOne: Record<string, {variable: string; value: string}> = {};
+      const resultOne = this.getListValueFromTextAreaOne(item.textAreaOne);
+      resultOne.forEach((item: string) => {
+        const itemSplit = item.split(':');
+        if(itemSplit.length >= 2) {
+          const key = itemSplit[0].replace('$', '');
+          objectMapOne[key] = {
+            variable: itemSplit[0],
+            value: itemSplit[1]
+          }
         }
-      }
-    });
+      });
+  
+      const objectMapTwo: Record<string, string> = {};
+      const resultTwo = this.getListValueFromTextAreaTwo(item.textAreaTwo);
+      resultTwo.forEach((item: string) => {
+        const itemSplit = item.split(':');
+        if(itemSplit.length >= 2) {
+          const key = itemSplit[0];
+          if(!objectMapOne[itemSplit[1].replace('$', '')]) {
+            objectMapTwo[key] = itemSplit[1];
+           return;
+          }
+          objectMapTwo[key] = objectMapOne[itemSplit[1].replace('$', '')]?.value;
+        }
+      });
+  
+      const objectMapThree: Record<string, {variable: string, name: string, pixelValue: string; hasUnit: boolean;}> = {};
+      const resultThree = this.getListValueFromTextAreaThree(item.textAreaThree);
+      resultThree.forEach((item: string) => {
+  
+        const itemSplit = item.split(':');
+        if(itemSplit.length >= 2) {
+          const key = itemSplit[0].replace('$', '');
+          if(!objectMapTwo[itemSplit[1]]) {
+            return;
+          }
+          objectMapThree[key] = {
+            name: itemSplit[1],
+            variable: itemSplit[0],
+            pixelValue: this.getValue(objectMapTwo[itemSplit[1]])?.toString(),
+            hasUnit: this.getUnit(objectMapTwo[itemSplit[1]])
+          };
+        }
+      })
+      Object.values(objectMapThree).forEach(({ name, variable, pixelValue, hasUnit }: { name: string; variable: string, pixelValue: string, hasUnit: boolean }) => {
+        this.result[`${item.prefix}-${name.replace('\"','').replace('\"','')}-${pixelValue}${hasUnit? 'px' : ''}`]= {
+          prefix: `${item.prefix}-${name.replace('\"','').replace('\"','')}-${pixelValue}${hasUnit? 'px' : ''}`,
+          body: [
+          `${item.attributeCss}: \\${variable};`
+          ],
+          description: `${item.attributeCss} for ${name.replace('\"','').replace('\"','')} with value ${hasUnit ? `${+pixelValue/+this.valueForm.pixel}rem = ${pixelValue}px` : pixelValue}`
+        }
+      });
 
-    const objectMapTwo: Record<string, number> = {};
-    const resultTwo = this.getListValueFromTextAreaTwo();
-    resultTwo.forEach((item: string) => {
-      const itemSplit = item.split(':');
-      if(itemSplit.length >= 2) {
-        const key = itemSplit[0];
-        objectMapTwo[key] = objectMapOne[itemSplit[1].replace('$', '')]?.value || 0;
-      }
-    });
-    const objectMapThree: Record<string, {variable: string, pixelValue: number;}> = {};
-    const resultThree = this.getListValueFromTextAreaThree();
-    resultThree.forEach((item: string) => {
+      this.clipboardResult[item.prefix] = this.result;
 
-      const itemSplit = item.split(':');
-      if(itemSplit.length >= 2) {
-        const key = itemSplit[0].replace('$', '');
-        objectMapThree[key] = {
-          variable: itemSplit[0],
-          pixelValue: objectMapTwo[itemSplit[1]] || 0
-        };
-      }
-    })
-    Object.entries(objectMapThree).forEach(([key, { variable, pixelValue }]: [string, { variable: string, pixelValue: number }]) => {
-      this.result[`${this.valueForm.prefix}-${key}-${pixelValue}px`] = {
-        prefix: `${this.valueForm.prefix}-${key}-${pixelValue}px`,
-        body: [
-    		`${this.valueForm.attributeCss}:${variable};`
-    	  ],
-        description: `${this.valueForm.attributeCss} for ${key} with value ${pixelValue/+this.valueForm.pixel}rem = ${pixelValue}px`
-      }
+    });
+  }
+
+  copyToClipboard() {
+    const textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.opacity = '0';
+    let text = '';
+    Object.entries(this.clipboardResult).forEach(([key, value]: [string, any]) => {
+      text += `//  Start  //\n\n// ${key} \n\n ${JSON.stringify(value, null, 2)} \n\n//  End  //`
+    });
+    textarea.value = text;
+
+    document.body.appendChild(textarea);
+
+    // Select the text in the textarea
+    textarea.select();
+    const successful = document.execCommand('copy');
+
+    document.body.removeChild(textarea);
+
+    return successful;
+  }
+
+  handleDelete(index: number) {
+    this.valueForm.values.splice(index, 1)
+  }
+  handleAdd() {
+    this.valueForm.values.push({
+      prefix: '',
+      attributeCss: '',
+      textAreaOne: '',
+      textAreaTwo: '',
+      textAreaThree: ''
     });
   }
 }
